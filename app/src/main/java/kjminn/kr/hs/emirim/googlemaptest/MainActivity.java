334043524c75f783fd1816d47e10667c374f3c3f
Kyoungju Minn <kjminn2001@gmail.com>
2017-06-27 20:47:43 +0900

    #6 Ground Overay Options(location mark icon)

--- app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/MainActivity.java
+++ app/src/main/java/kjminn/kr/hs/emirim/googlemaptest/MainActivity.java
@@ -10,11 +10,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
     GoogleMap googleMap;
     SupportMapFragment mapFragment;
+    GroundOverlayOptions loc_mark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,17 +28,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
         this.googleMap=googleMap;
         googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(35.839269, 128.631892), 17));
         googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                loc_mark=new GroundOverlayOptions();
+                loc_mark.image(BitmapDescriptorFactory.fromResource(R.drawable.loc_icon)).position(latLng, 100f, 100f);
+                googleMap.addGroundOverlay(loc_mark);
+            }
+        });
     }
 
     public static final int ITEM_SATELLITE=1;
     public static final int ITEM_NORMAL=2;
     public static final int ITEM_COEXMALL=3;
     public static final int ITEM_IPARKMALL=4;
+    public static final int ITEM_MARK_CLEAR=5;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,6 +57,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         SubMenu hotMenu=menu.addSubMenu("핫 플레이스");
         hotMenu.add(0, ITEM_COEXMALL, 0, "코엑스몰");
         hotMenu.add(0, ITEM_IPARKMALL, 0, "아이파크몰");
+        menu.add(0, ITEM_MARK_CLEAR, 0, "위치 아이콘 제거");
         return true;
     }
 
@@ -59,6 +72,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             case ITEM_COEXMALL: googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.513287, 127.057760), 17));
                 return true;
             case ITEM_IPARKMALL: googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.529084, 126.963991), 17));
+                return true;
+            case ITEM_MARK_CLEAR: googleMap.clear();
                 return true;
         }
         return false;
